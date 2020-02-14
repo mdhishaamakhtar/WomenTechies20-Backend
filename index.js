6420d6a3919f921f696d4b8894eb7ac5d9bb3cf2
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2020-02-15 00:41:37 +0530

    Project Ready

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-var whitelist = ['https://womentechies.dscvit.com','https://prakhar0912.github.io'];
+var whitelist = ['https://womentechies.dscvit.com', 'https://prakhar0912.github.io'];
 var corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
     }
-  }
 };
 
 
