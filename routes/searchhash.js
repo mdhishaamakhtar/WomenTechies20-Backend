5943e2593291dea2af2120f7962d91d058bdb65d
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2020-02-25 00:13:49 +0530

    Insta API

--- routes/searchhash.js
+++ routes/searchhash.js
@@ -6,22 +6,15 @@ var access_token = process.env.ACCESS_TOKEN;
 var user_id = process.env.USER_ID;
 //make a post request to get the hashtag required
 router.post("/", verify, (req, res) => {
-  var hashtag = req.body.hashtag;
-  var baseURL = "https://graph.facebook.com/ig_hashtag_search?user_id=";
-  var finalURL =
+  let hashtag = req.body.hashtag;
+  let baseURL = "https://graph.facebook.com/ig_hashtag_search?user_id=";
+  let finalURL =
     baseURL + user_id + "&q=" + hashtag + "&access_token=" + access_token;
   //make requst to get id of the hashtag
   request(finalURL, function(error, response, body) {
-    const hashtags = JSON.parse(body).data;//get the id of the hashtag
-    //use the id in the 
-    
-    let hashtagID;
-    if (hashtags.length > 0) {
-      hashtagID = hashtags[0].id;
-    } else {
-      return res.json({message:"Instagram API Error", error: error}).status(500);
-    }
-    var url =
+    let hashtagID = JSON.parse(body).data[0].id;//get the id of the hashtag
+    //use the id in the url
+    let url =
       "https://graph.facebook.com/" +
       hashtagID +
       "/recent_media?user_id=" +
